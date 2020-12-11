7b3a44b6a6986a730b17cac57e3966f6a132fe03
alighasemi <alighasemi_1376@yahoo.com>
2020-12-12 00:47:43 +0330

    gg

--- Problems/Right triangle/main.py
+++ Problems/Right triangle/main.py
@@ -4,7 +4,7 @@ class RightTriangle:
         self.a = leg_1
         self.b = leg_2
         # calculate the area here
-        self.s = (1/2) * leg_1 * leg_2
+        self.s = (1 / 2) * leg_1 * leg_2
 
 
 # triangle from the input
@@ -13,7 +13,6 @@ input_c, input_a, input_b = [int(x) for x in input().split()]
 # write your code here
 if (input_c ** 2) == (input_a ** 2) + (input_b ** 2):
     g = RightTriangle(input_c, input_a, input_b)
-    print(g)
     print(g.s)
 else:
     print('Not right')
